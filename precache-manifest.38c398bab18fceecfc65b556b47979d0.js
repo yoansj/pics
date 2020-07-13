fa6b090e5a193f9314af6b0b5c254d6b7ab2f92e
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2020-07-13 22:56:44 +0200

    Updates

---
+++ precache-manifest.38c398bab18fceecfc65b556b47979d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae956997f104c746cf238f1d5378fc2",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "51a0a8fb7c2670be415b",
+    "url": "/pics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f340f5ff830720d7fd01",
+    "url": "/pics/static/js/2.9eb332b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.9eb332b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a0a8fb7c2670be415b",
+    "url": "/pics/static/js/main.2458655b.chunk.js"
+  },
+  {
+    "revision": "24445b424d0ed337bcc7",
+    "url": "/pics/static/js/runtime-main.db0de8af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pics/static/media/logo.5d5d9eef.svg"
+  }
+]);
